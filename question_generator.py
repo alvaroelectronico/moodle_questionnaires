68a1a991e6529486a6be8f7479bc987975cc2d42
Álvaro García Sánchez <alvaroelectronico@gmail.com>
2021-12-22 11:22:43 +0100

    create_xml_elem receives a list with the information of the questions

--- question_generator.py
+++ question_generator.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from importlib.metadata import files
 from unicodedata import category
 from xml.etree import ElementTree
@@ -12,8 +15,8 @@ from torch.utils.collect_env import check_release_file
 def prettify(elem):
     """Return a pretty-printed XML string for the Element.
     """
-    rough_string = ElementTree.tostring(elem, encoding='unicode')
-    #rough_string = ElementTree.tostring(elem, encoding='UTF-8) #Python2
+    #rough_string = ElementTree.tostring(elem, encoding='unicode')
+    rough_string = ElementTree.tostring(elem, encoding='UTF-8') #Python2
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
@@ -24,7 +27,7 @@ def save_to_file(elem, file_name="output_xml"):
     file.close()
 
 
-def create_xml_elem(category_name_str, question_name_str, question_text_str, answer_str, tolerance_str):
+def create_xml_elem(category_name_str, question_data):
     quiz = Element("quiz")
 
     # Question comment
@@ -40,27 +43,30 @@ def create_xml_elem(category_name_str, question_name_str, question_text_str, ans
     info_text = SubElement(info, "text")
     info_text.text = ""
 
-    # Type of question is numerical
-    question = SubElement(quiz, "question", type="numerical")
 
-    # Question name: the name that will be seen in Moodle
-    question_name = SubElement(question, "name")
-    question_name_text = SubElement(question_name, "text")
-    question_name_text.text = question_name_str
+    for question_name_str, question_text_str, answer_str, tolerance_str in question_data:
 
-    # Question text: what the student has to reply to
-    question_text = SubElement(question, "questiontext", format='html')
-    question_text_text = SubElement(question_text, "text")
-    question_text_text.text = question_text_str
+        # Type of question is numerical
+        question = SubElement(quiz, "question", type="numerical")
 
-    # Correct value of the answer
-    answer = SubElement(question, "answer", fraction="100", fomrat="moodle_auto_format")
-    answer_text = SubElement(answer, "text")
-    answer_text.text = answer_str
+        # Question name: the name that will be seen in Moodle
+        question_name = SubElement(question, "name")
+        question_name_text = SubElement(question_name, "text")
+        question_name_text.text = question_name_str
 
-    # Tolerance value
-    tolerance_text = SubElement(answer, "tolerance")
-    tolerance_text.text = tolerance_str
+        # Question text: what the student has to reply to
+        question_text = SubElement(question, "questiontext", format='html')
+        question_text_text = SubElement(question_text, "text")
+        question_text_text.text = question_text_str
+
+        # Correct value of the answer
+        answer = SubElement(question, "answer", fraction="100", format="moodle_auto_format")
+        answer_text = SubElement(answer, "text")
+        answer_text.text = answer_str
+
+        # Tolerance value
+        tolerance_text = SubElement(answer, "tolerance")
+        tolerance_text.text = tolerance_str
 
     return quiz
 
@@ -71,12 +77,12 @@ question_text_str = "Enunciado de la pregunta"
 answer_str = "10"
 tolerance_str = "1"
 
+question_data = [[question_name_str, question_text_str, answer_str, tolerance_str]]
+
 # Creating the ET elment to be prettified
-quiz = check_release_file(category_name_str, question_name_str, question_text_str, answer_str, tolerance_str)
+quiz = create_xml_elem("tutorial AIMMS", question_data)
 
 # Prettifying and saving the pretty string to an .xml file
-save_to_file(quiz, "prueba.xml")
+save_to_file(quiz, "prueba_202122.xml")
 
 print(prettify(quiz))
-
-
