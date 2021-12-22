633802c7d7612dfe696d3371f608fe8249904bd3
Álvaro García Sánchez <alvaroelectronico@gmail.com>
2021-12-22 13:23:54 +0100

    Sanity check commit

--- question_generator.py
+++ question_generator.py
@@ -16,7 +16,7 @@ def prettify(elem):
     """Return a pretty-printed XML string for the Element.
     """
     #rough_string = ElementTree.tostring(elem, encoding='unicode')
-    rough_string = ElementTree.tostring(elem, encoding='UTF-8') #Python2
+    rough_string = ElementTree.tostring(elem, encoding='utf-8') #Python2
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
