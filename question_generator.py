4d399f06c10b2cbd4f427365470370752ae48068
Álvaro García Sánchez <alvaroelectronico@gmail.com>
2021-12-24 12:34:06 +0100

    Code better documented

--- question_generator.py
+++ question_generator.py
@@ -13,14 +13,22 @@ from torch.utils.collect_env import check_release_file
 
 
 def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
+    """
+    :param elem: Element previously generated with ElementTree
+    :return: pretty-printed XML string for the Element.
     """
     #rough_string = ElementTree.tostring(elem, encoding='unicode')
     rough_string = ElementTree.tostring(elem, encoding='utf-8') #Python2
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
+
 def save_to_file(elem, file_name="output_xml"):
+    """
+    :param elem: Element previously generated with ElementTree
+    :param file_name: this is the file name of the gerenated file
+    :return: a file with the prettified string obtained from the element elem
+    """
     string = prettify(elem)
     file = open(file_name, "w")
     file.write(string)
@@ -28,6 +36,17 @@ def save_to_file(elem, file_name="output_xml"):
 
 
 def create_xml_elem(category_name_str, question_data):
+    """
+    This function creates an Element with information for serveral Moodle questions corresponding to a give
+    category
+    :param category_name_str: the name of the category in Moodle the questions belong to.
+    :param question_data: this a list of lists, where each list is has four elements:
+        question_data[0] is the question name which will be used to store it in Moodle
+        question_data[1] text is the question text itself
+        question_data[2] is the correct value for the question
+        question_data[3] is the tolerance for the correct value
+    :return: Element type (of ElementTree).
+    """
     quiz = Element("quiz")
 
     # Question comment
@@ -70,19 +89,23 @@ def create_xml_elem(category_name_str, question_data):
 
     return quiz
 
-# Data for building the question
-category_name_str = "category name"
-question_name_str = "pregunta prueba 1"
-question_text_str = "Enunciado de la pregunta"
-answer_str = "10"
-tolerance_str = "1"
+"""
+What follows is code to check that the previous functions work fine.
+When using this module from another .py file, importing the functions would be enough.
+That other .py file should create the lists for create_xml_elem function to be run
+"""
 
-question_data = [[question_name_str, question_text_str, answer_str, tolerance_str]]
+import numpy as np
+
+# Data for building the question
+no_test_questions = 10
+question_data = [["question_test_{}".format(i),
+                 "text_{}".format(i),
+                 "{}".format(100*np.random.random()),
+                 "{}".format(10*np.random.random())]
+                 for i in range(10)]
 
 # Creating the ET elment to be prettified
-quiz = create_xml_elem("tutorial AIMMS", question_data)
-
+quiz = create_xml_elem("test_category", question_data)
 # Prettifying and saving the pretty string to an .xml file
 save_to_file(quiz, "prueba_202122.xml")
-
-print(prettify(quiz))
